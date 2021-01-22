26f3aa13f7335a6f94bc000be971eb284e335964
gzh5668 <54763817+gzh5668@users.noreply.github.com>
2021-01-22 06:02:04 -0800

    comment added

--- 170 Prototype/Assets/Scripts/SwitchActive.cs
+++ 170 Prototype/Assets/Scripts/SwitchActive.cs
@@ -6,6 +6,8 @@ public class SwitchActive : MonoBehaviour {
   public GameObject hidedObj;
   private float timer;
 
+  // timer to set hidedObj back to true when not
+  // colliding with pressure plate
   private void Update() {
     if (timer > 0) {
       timer -= Time.deltaTime;
@@ -15,13 +17,16 @@ public class SwitchActive : MonoBehaviour {
     }
   }
 
+  // when pressure plate collide with enemy or Player
+  // set hidedObj to false
   private void OnTriggerEnter2D (Collider2D col) {
     if (col.tag == "Player" || col.tag == "Enemy") {
-      Debug.Log("button working");
+      // Debug.Log("pressure plate working");
       hidedObj.SetActive(false);
     }
   }
 
+  // check if the collison is still hapening
   private void OnTriggerStay2D (Collider2D col) {
    if (col.tag == "Player" || col.tag == "Enemy") {
      timer = 1f;
