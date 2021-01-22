17e1d76b9fc48b5c245f42c57b47be397320959c
gzh5668 <54763817+gzh5668@users.noreply.github.com>
2021-01-22 05:55:37 -0800

    level5 pressure plate working

--- 170 Prototype/Assets/Scripts/SwitchActive.cs
+++ 170 Prototype/Assets/Scripts/SwitchActive.cs
@@ -6,25 +6,26 @@ public class SwitchActive : MonoBehaviour {
   public GameObject hidedObj;
   private float timer;
 
-  //private void Update() {
-  //  if (timer > 0) {
-  //    timer -= Time.deltaTime;
-  //    if (timer <= 0f) {
-  //      hidedObj.SetActive(true);
-  //    }
-  //  }
-  //}
+  private void Update() {
+    if (timer > 0) {
+      timer -= Time.deltaTime;
+      if (timer <= 0f) {
+        hidedObj.SetActive(true);
+      }
+    }
+  }
 
   private void OnTriggerEnter2D (Collider2D col) {
-    if (col.tag == "Switch") {
+    if (col.tag == "Player" || col.tag == "Enemy") {
+      Debug.Log("button working");
       hidedObj.SetActive(false);
     }
   }
 
-  //private void OnTriggerStay2D (Collider2D col) {
-  // if (col.tag == "Switch") {
-  //   timer = 1f;
-  // }
-  //}
+  private void OnTriggerStay2D (Collider2D col) {
+   if (col.tag == "Player" || col.tag == "Enemy") {
+     timer = 1f;
+   }
+  }
 
 }
