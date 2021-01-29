4db13c91c35d2797794a2b21dc33cdf924080e02
jerjwei <jerjwei@gmail.com>
2021-01-28 17:47:02 -0800

    Ice platform update
    Jump implemented.
    New movement on ice method implemented.

--- 170 Prototype/Assets/Scripts/Scene Ice Scripts/MovementIce.cs
+++ 170 Prototype/Assets/Scripts/Scene Ice Scripts/MovementIce.cs
@@ -5,34 +5,102 @@ using System;
 
 public class MovementIce : MonoBehaviour
 {
-    public Rigidbody2D rb;      // Rigidbody variable
-    public float speed;         // player movement speed 
+    // Horizontal Movement Variables
+    public Rigidbody2D rb;              // Rigidbody variable
+    [Range(0, 5)]
+    public float movementSpeed;         // Player movement movementSpeed 
+    [Range(0, 5)]
+    public float speedOnIce;            // Movement speed on ice
+    public float Direction;             // Player's direction choice after collide with somthin on ice
+    public bool onIce;                  // If player is on ice
+    public bool onLeft;                 // If player touch left boundary
+    public bool onRight;                // If player touch right boundary
+
+    // Vertical Movement Variables
+    public float jumpForce = 20f;
+    [Range(0,1)]
+    public float jumpHeightReduce = 0.5f;
+    private bool groundCheck2 = true;
+
+    //Platform Collision Variables
+    public Transform feet;
+    public LayerMask groundLayers;
+    public Collider2D player;
+    public GameObject dialogueBox;
+
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
-        //Movement();
-    }
-
-    void Movement(){
-        // Get One Direction^^
-        float horizontalmove;
-        horizontalmove = Input.GetAxis("Horizontal");
-        Debug.Log("horizontalmove = " + horizontalmove);
-
-        // PlayerMovement method
-        if(horizontalmove != 0){
-            Debug.Log("Not on ice");
-            rb.velocity = new Vector2(horizontalmove * speed, rb.velocity.y);
+        //Check if Space is pressed down and touching the ground at the same time
+        if(Input.GetButtonDown("Jump") && IsGrounded()){
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            groundCheck2 = true;
+            
+            //comment out for future need --- Access from playerMovement code to enemyMovement variable
+            //monsterCanMove.canMove = !(monsterCanMove.canMove);
+        }
+        else if(groundCheck2 == false){
+            groundCheck2 = (Physics2D.OverlapBox(feet.position, new Vector2(0.25f, .5f), 0f, groundLayers) != null);
+        }
+        //Check if Space is released up before it reached the maximum jump height
+        if(Input.GetButtonUp("Jump") && rb.velocity.y > 0){
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * jumpHeightReduce);
         }
     }
 
-    void MovementAfterIceR(){
+    void FixedUpdate(){
+        // Get horizontal velocity
+        float horizontalmove = rb.velocity.x*movementSpeed;
+        horizontalmove += Input.GetAxisRaw("Horizontal");
+        Debug.Log("horizontalmove = " + horizontalmove);
+        // Direction play choose after collide something on ice
+        Direction = Input.GetAxisRaw("Horizontal");
+
+        // PlayerMovement method (Based on if player stand on the ice)
+        if(horizontalmove != 0 && !onIce){                                          // If not on ice, do normal movement
+            Debug.Log("Not on ice");
+            rb.velocity = new Vector2(horizontalmove, rb.velocity.y);
+        }else if(onIce){                                                            // if on ice, do sliding movement
+            Debug.Log("On Ice");
+            rb.velocity = new Vector2(speedOnIce, rb.velocity.y);
+            if(onRight&&Direction == -1){                                           
+                speedOnIce *= -1;
+                onRight = false;
+                Debug.Log("in right fun");
+            }
+            if(onLeft&&Direction == 1){
+                speedOnIce *= -1;
+                onLeft = false;
+            }
+
+            //Check if Space is pressed down and touching the ground at the same time
+            if(Input.GetButtonDown("Jump") && IsGrounded()){
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                groundCheck2 = true;
+                
+                //comment out for future need --- Access from playerMovement code to enemyMovement variable
+                //monsterCanMove.canMove = !(monsterCanMove.canMove);
+            }
+            else if(groundCheck2 == false){
+                groundCheck2 = (Physics2D.OverlapBox(feet.position, new Vector2(0.25f, .5f), 0f, groundLayers) != null);
+            }
+            //Check if Space is released up before it reached the maximum jump height
+            if(Input.GetButtonUp("Jump") && rb.velocity.y > 0){
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * jumpHeightReduce);
+            }
+        }
+    }
+
+    /*void MovementAfterIceR(){
         float horizontalmove;
         horizontalmove = Input.GetAxisRaw("Horizontal");
         Debug.Log("horizontalmove right ice = " + horizontalmove);
         // PlayerMovement Method
-        if(horizontalmove == -1){
-            speed *= -1;
+        if(horizontalmove < 0){
+            movementSpeed *= -1;
         }
     }
 
@@ -41,34 +109,35 @@ public class MovementIce : MonoBehaviour
         horizontalmove = Input.GetAxisRaw("Horizontal");
         Debug.Log("horizontalmove left ice = " + horizontalmove);
         // PlayerMovement Method
-        if(horizontalmove == 1){
-            speed *= -1;
+        if(horizontalmove > 0){
+            movementSpeed *= -1;
         }
-    }
+    }*/
 
-    // Collision detector, returns true while player keep coliding the ice
+    // Collision detector
     private void OnCollisionStay2D(Collision2D col){
         // if collide with ice do sliding
         if(col.gameObject.tag == "Ice"){
-            rb.velocity = new Vector2(speed, -10);
+            onIce = true;
         // if not collide do normal movement
         }else if(col.gameObject.tag == "WallR"){
-            MovementAfterIceR();
+            Debug.Log("onRight = " + onRight);
+            onRight = true;
+            Debug.Log("onRight = " + onRight);
         }else if(col.gameObject.tag == "WallL"){
-            MovementAfterIceL();
+            Debug.Log("onLeft = " + onLeft);
+            onLeft = true;
+            Debug.Log("onLeft = " + onLeft);
         }else{
-            Movement();
+            onIce = false;
         }
   }
 
-    /*private void OnCollisionEnter2D(Collision2D col){
-        if(col.gameObject.tag == "WallR"){
-            Debug.Log("Touching right wall");
-            MovementAfterIce();
-        }
-        if(col.gameObject.tag == "WallL"){
-            Debug.Log("Touching left wall");
-        }
-    }*/
+    //check for if touch ground
+    public bool IsGrounded(){
+        Collider2D groundCheck = Physics2D.OverlapBox(feet.position, new Vector2(3.5f, 1f), 0f, groundLayers);
+        return (groundCheck != null && groundCheck2 != false);
+  }
+
 }
 
