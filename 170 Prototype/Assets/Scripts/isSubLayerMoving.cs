01731bcca4b74fe9a465b7b60950adc148a92525
Saif <skausar@ucsc.edu>
2021-02-02 01:13:23 -0800

    Found and fixed some bugs in level 1 and 2

--- 170 Prototype/Assets/Scripts/isSubLayerMoving.cs
+++ 170 Prototype/Assets/Scripts/isSubLayerMoving.cs
@@ -7,12 +7,9 @@ public class isSubLayerMoving : MonoBehaviour
     public GameObject layer;
     float initialy;
     // Update is called once per frame
-    void Start(){
-      initialy = layer.transform.position.y;
-    }
     void Update()
     {
-      if(layer.transform.position.y != 0f && layer.transform.position.y != initialy){
+      if(layer.transform.position.y != 0f){
         foreach (Transform t in layer.transform) {
           t.GetComponent<EdgeCollider2D>().enabled = false;
         }
