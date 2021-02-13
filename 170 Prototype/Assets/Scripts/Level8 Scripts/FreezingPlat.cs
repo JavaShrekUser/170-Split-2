748b478302d6f73d56cb157af60226d914aa3d60
jerjwei <jerjwei@gmail.com>
2021-02-12 17:09:08 -0800

    Freezing ice bug fixed
    Player will now be able to move on the normal ground before they collide w/ water.

--- 170 Prototype/Assets/Scripts/Level8 Scripts/FreezingPlat.cs
+++ 170 Prototype/Assets/Scripts/Level8 Scripts/FreezingPlat.cs
@@ -7,12 +7,15 @@ public class FreezingPlat : MonoBehaviour
   public GameObject disappearingGround;
   public GameObject showingGround;
 
-  private void OnTriggerEnter2D(Collider2D col){
+  private void OnCollisionEnter2D(Collision2D col){
 
     // If touching the water particles, the dG will disappear and sG will show up.
-    if(col.gameObject.layer == 4){
+    if(col.gameObject.tag == "Metaball_liquid"){
         Debug.Log("touchin plat");
         disappearingGround.SetActive(false);
+        showingGround.SetActive(true);
+      }else{
+        Debug.Log("not touchin plat");
         showingGround.SetActive(true);
       }
 
