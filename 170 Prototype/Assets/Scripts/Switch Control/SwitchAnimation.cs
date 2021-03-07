6495a05f0fa3826a102377218c407bb0e1c8e407
bowenzhao <bzhao19@ucsc.edu>
2021-03-06 22:43:51 -0500

    button animation implemented
    normal up, normal down, step on, recovery, step on then leave transition to recovery, middle recovery and step on transition to step on animation

---
+++ 170 Prototype/Assets/Scripts/Switch Control/SwitchAnimation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchAnimation : MonoBehaviour
+{
+
+    //Animation movements
+    public Animator animator;
+
+    public float timer = .1f;
+    private float cdTimer;
+    private bool buttonTouch = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cdTimer > 0) 
+        {
+            cdTimer -= .1f;
+            if (cdTimer <= 0f) {
+                buttonTouch = false;
+            }
+        }
+
+        animator.SetBool("Touching", buttonTouch);
+
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.tag == "Player" || col.tag == "Enemy" || col.tag == "Trap") 
+        {
+            buttonTouch = true;
+            cdTimer = timer;
+        }
+
+    }
+}
