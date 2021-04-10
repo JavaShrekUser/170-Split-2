05e80def659b6dc59979b5d6338ada9f9008f1cd
bowenzhao <bzhao19@ucsc.edu>
2021-04-10 18:16:14 -0400

    lvl 10 implement 2 button

---
+++ 170 Prototype/Assets/Scripts/Switch Control/SwitchHideLvl10.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchHideLvl10 : MonoBehaviour{
+
+public GameObject showObj1;
+public GameObject showObj2;
+public GameObject showObj3;
+public GameObject showObj4;
+public GameObject showObj5;
+  public float timer = 1f;
+  private float cdTimer;
+  // timer to set showObj back to true when not
+  // colliding with pressure plate
+  private void Update() {
+    if (cdTimer > 0) {
+      cdTimer -= Time.deltaTime;
+      if (cdTimer <= 0f) {
+        showObj1.SetActive(true);
+        showObj2.SetActive(true);
+        showObj3.SetActive(true);
+        showObj4.SetActive(true);
+        showObj5.SetActive(true);
+      }
+    }
+  }
+
+  // when pressure plate collide with enemy or Player
+  // set showObj to false
+  private void OnTriggerEnter2D (Collider2D col) {
+    if (col.tag == "Player" || col.tag == "Enemy" || col.tag == "Trap" || col.tag == "Metaball_liquid") {
+      // Debug.Log("pressure plate working");
+        showObj1.SetActive(false);
+        showObj2.SetActive(false);
+        showObj3.SetActive(false);
+        showObj4.SetActive(false);
+        showObj5.SetActive(false);
+    }
+  }
+
+  // check if the collison is still hapening
+  private void OnTriggerStay2D (Collider2D col) {
+   if (col.tag == "Player" || col.tag == "Enemy" || col.tag == "Trap" || col.tag == "Metaball_liquid") {
+     cdTimer = timer;
+   }
+  }
+
+}
---
+++ 170 Prototype/Assets/Scripts/Switch Control/SwitchStopLvl10.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchStopLvl10 : MonoBehaviour
+{
+    public Rigidbody2D rb1;
+    public Rigidbody2D rb2;
+    public float timer = 1f;
+    private float cdTimer;
+    // timer to set showObj back to true when not
+    // colliding with pressure plate
+    private void Update()
+    {
+        if (cdTimer > 0)
+        {
+
+            cdTimer -= Time.deltaTime;
+            if (cdTimer <= 0f)
+            {
+                rb1.constraints = RigidbodyConstraints2D.FreezeAll;
+                rb2.constraints = RigidbodyConstraints2D.FreezeAll;
+                /*rb.constraints =
+                    RigidbodyConstraints2D.FreezePositionY |
+                    RigidbodyConstraints2D.FreezeRotation;*/
+            }
+        }
+    }
+
+    // when pressure plate collide with enemy or Player
+    // set showObj to false
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player" || col.tag == "Metaball_liquid")
+        {
+            // Debug.Log("pressure plate working");
+            //rb.constraints = RigidbodyConstraints2D.FreezeAll;
+            rb1.constraints =
+                    RigidbodyConstraints2D.FreezePositionY |
+                    RigidbodyConstraints2D.FreezeRotation;
+            rb2.constraints =
+                    RigidbodyConstraints2D.FreezePositionY |
+                    RigidbodyConstraints2D.FreezeRotation;        
+        }
+    }
+
+    // check if the collison is still hapening
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.tag == "Player" || col.tag == "Metaball_liquid")
+        {
+            cdTimer = timer;
+        }
+    }
+
+}
