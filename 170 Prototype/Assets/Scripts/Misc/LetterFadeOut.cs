35bbd6418b7e4e4493b7ef3181dd7674709fc731
Saif <skausar@ucsc.edu>
2021-04-26 13:37:56 -0700

    Added Tutorial and blured background

---
+++ 170 Prototype/Assets/Scripts/Misc/LetterFadeOut.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterFadeOut : MonoBehaviour
+{
+    public GameObject Fragment;
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(Fragment.activeSelf == false){
+        anim.SetTrigger("Active");
+      }
+    }
+}
