05b1fa0aa20f88e2bec16e4f3637a7ed865468e6
Saif Kausar <skausar@ucsc.edu>
2021-05-11 12:52:56 -0700

    Added in Dialogue to first couple levels

--- 170 Prototype/Assets/Scripts/DialogNew.cs
+++ 170 Prototype/Assets/Scripts/DialogNew.cs
@@ -3,21 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+
 public class DialogNew : MonoBehaviour
 {
   public TextMeshProUGUI textDisplay;
   public string[] sentences;
   private int index;
   public float typingSpeed;
+  public GameObject box;
+  public GameObject cam;
 
   private bool next = false;
+  private bool start = true;
 
-  void Start()
-  {
-    StartCoroutine(Type());
-  }
   void Update()
   {
+    if(index == 0 && box.transform.position.y == 0 && start){
+      start = false;
+      StartCoroutine(Type());
+    }
     if(Input.anyKeyDown && next){
       NextSentence();
     }
@@ -53,6 +57,7 @@ public class DialogNew : MonoBehaviour
     }
     else{
       textDisplay.text = "";
+      cam.GetComponent<CameraFadeOut>().Reset();
     }
   }
 
---
+++ 170 Prototype/Assets/Scripts/Misc/CameraFadeOut.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraFadeOut : MonoBehaviour
+{
+  public AnimationCurve FadeCurve = new AnimationCurve(new Keyframe(0, 1), new Keyframe(0.6f, 0.7f, -1.8f, -1.2f), new Keyframe(1, 0));
+
+  private float _alpha = 1;
+  private Texture2D _texture;
+  private bool _done = true;
+  private float _time;
+
+  public void Reset()
+  {
+      _done = false;
+      _alpha = 0;
+      _time = 0;
+  }
+
+  public void OnGUI()
+  {
+      if (_done) return;
+      if (_texture == null) _texture = new Texture2D(1, 1);
+
+      _texture.SetPixel(0, 0, new Color(0, 0, 0, _alpha));
+      _texture.Apply();
+
+      _time += Time.deltaTime/1.5f;
+      _alpha = 1-FadeCurve.Evaluate(_time);
+      GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), _texture);
+
+      if (_alpha >= 1){
+         _done = true;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+  }
+}
