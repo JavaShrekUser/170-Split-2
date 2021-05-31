35735eb3ecf81935d009083df2dcec500fd34fae
Saif Kausar <skausar@ucsc.edu>
2021-05-31 02:53:57 -0700

    more polishing for first few levels

--- 170 Prototype/Assets/Scripts/Misc/FragmentHover.cs
+++ 170 Prototype/Assets/Scripts/Misc/FragmentHover.cs
@@ -4,25 +4,9 @@ using UnityEngine;
 
 public class FragmentHover : MonoBehaviour
 {
-    float startingY;
-    bool move;
-    void Start(){
-      startingY = transform.position.y;
-    }
     // Update is called once per frame
     void Update()
     {
-      if(transform.position.y - startingY >= 0.60f){
-        move = true;
-      }
-      else if(transform.position.y <= startingY){
-        move = false;
-      }
-      if(move){
-        transform.Translate(new Vector3(0f,-0.0005f,0f));
-      }
-      else{
-        transform.Translate(new Vector3(0f,0.0005f,0f));
-      }
+      transform.Translate( new Vector3(0f,0.0005f,0f) * Mathf.Cos(Time.time));
     }
 }
