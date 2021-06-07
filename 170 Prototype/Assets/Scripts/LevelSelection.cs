38f512906f9d4a64ba53cd7323b5461249baba4e
Abdulla Askar <63617982+abdullaaskar@users.noreply.github.com>
2021-06-06 22:47:09 -0700

    Level Selection Core Script

---
+++ 170 Prototype/Assets/Scripts/LevelSelection.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelection : MonoBehaviour
+{
+    public void Level1()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void Level2()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void Level3()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level3");
+    }
+
+    public void Level4()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level4");
+    }
+
+    public void Level5()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level5");
+    }
+
+    public void Level6()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level6");
+    }
+
+    public void Level7()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level7");
+    }
+
+    public void Level8()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level8");
+    }
+
+    public void Level9()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level9");
+    }
+
+    public void Level10()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level10");
+    }
+
+    public void Level11()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level11");
+    }
+
+    public void Level12()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level12");
+    }
+
+    public void Level13()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level13");
+    }
+
+    public void Level14()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level14");
+    }
+
+    public void Level15()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level15");
+    }
+
+    public void Level16()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level16");
+    }
+
+    public void Level17()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level17");
+    }
+
+    public void Level18()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level18");
+    }
+
+    public void Level19()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level19");
+    }
+
+    public void Level20()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level20");
+    }
+}
